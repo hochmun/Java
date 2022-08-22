2d5f2111b22109fd0fe6b45f1596d5f8edcb5306
hochmun <s01099760587@gmail.com>
2022-08-22 11:03:52 +0900

    backjoon add, memo add

---
+++ Backjoon/src/Step05/_01.java
@@ -0,0 +1,15 @@
+package Step05;
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 심규영
+ * 내용 : 백준 5단계 1번째 문제, 정수 N개의 합
+ */
+public class _01 {
+	long sum(int[] a) {
+		long sum = 0;
+		for(int i =0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+}
